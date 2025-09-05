6faed67432c61eaab185ccb9968301e21d1ec77c
FEDERICO VICTOR ABAS <49121720@ort>
2025-09-05 15:56:56 -0300

    wfwf

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,38 @@
-hola()
+
+const input = document.getElementById("opciones")
+const resultado = document.getElementById("resultado")
+
+function calculadora(nombre, fecha)
+{
+    input.innerHTML="<input type ='date' id = 'fecha'>"
+    input.innerHTML="<input type ='text' id = 'nombre'>"
+    input.innerHTML="<button onclick ='calculadora()'>Enviar></button>"
+    let nombre = document.getElementById("nombre").value;
+    let fechaNacimiento = document.getElementById("fecha").value;
+
+
+    let fechaHoy = new Date()
+    añoHoy = fechaHoy.getFullYear()
+    let fechaDate = Date.parse(fecha)
+    añoDate = añoDate.getFullYear()
+    edad = añoHoy - añoDate
+    return edad
+      
+}
+function ej1()
+{
+
+
+
+}
+function tirando()
+{
+    let arrayFrutas = ["banana", "anana", "manzana", "kiwi", "pera" ,"damasco" ,"naranja", "mandarina", "uva","cereza"]
+    for(let fruta of arrayFrutas) 
+    {  console.log(`${fruta}`);  }
+}
+function comparar()
+{
+    let comparacion1 = 10 == 10
+    let comparacion2 = 10 === 10
+}
